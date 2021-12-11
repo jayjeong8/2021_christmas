1cb60abd37390080265c6bf14cacbf9ec88ce432
jayjeong8 <82160479+jayjeong8@users.noreply.github.com>
2021-12-12 02:15:08 +0900

    ♻️ eliminate side effect

--- app.js
+++ app.js
@@ -1,57 +1,50 @@
-const input = document.querySelector('input');
-const eyes = document.querySelectorAll('.eye');
+const msgBox = document.querySelector('input');
+const eyesB = document.querySelectorAll('.eye');
 const eyesW = document.querySelectorAll('.eyeW');
 
-// function inputValueCheck(e) { //방법1, x값만 움직임
-// const eyesDefaultValue1 = 925.75;
-// const eyesDefaultValue2 = 996.75;
-//     const eyesValue = 4;
-//     const inputLength = e.target.value.length;
-//     const eyesMovingValue = 0.5 * inputLength - eyesValue;
-//
-//     if (inputLength === 0) {
-//         eyes[0].cx.baseVal.value = eyesDefaultValue1;
-//         eyes[1].cx.baseVal.value = eyesDefaultValue2;
-//     } else if (inputLength === 1) {
-//         eyes[0].cx.baseVal.value = eyesDefaultValue1 - eyesValue;
-//         eyes[1].cx.baseVal.value = eyesDefaultValue2 - eyesValue;
-//     } else if (inputLength < 16) {
-//         eyes[0].cx.baseVal.value = eyesDefaultValue1 + eyesMovingValue;
-//         eyes[1].cx.baseVal.value = eyesDefaultValue2 + eyesMovingValue;
-//     } else {
-//         eyes[0].cx.baseVal.value = eyesDefaultValue1 + eyesValue;
-//         eyes[1].cx.baseVal.value = eyesDefaultValue2 + eyesValue;
-//     }
-// }
+const msgBoxMarginSide = 0.41;
+const msgBoxMarginUpDown = 0.5;
+const msgBoxWidth = 0.16;
 
-function inputValueCheck2(e) {
-    const whiteEyePosition = {x: eyesW[0].cx.baseVal.value, y: eyesW[0].cy.baseVal.value};
-    const inputMarginSide = 0.41;
-    const inputMarginUpDown = 0.5;
-    const inputWidth = 0.16;
-    const inputY = document.body.clientHeight * inputMarginUpDown;
-    const inputPosition1 = {x: document.body.clientWidth * inputMarginSide, y: inputY};
-    // const inputPosition2 = {x: document.body.clientWidth * (inputMarginSide + inputWidth), y: inputY};
-    const inputCenterPosition = {x: document.body.clientWidth * (inputMarginSide + inputWidth / 2), y: inputY};
-
-    const sideCenter = inputCenterPosition.y - whiteEyePosition.y;
-    const sideBottomHalf = inputCenterPosition.x - inputPosition1.x;
-
-    const eyeRadius = eyesW[0].r.baseVal.value - eyes[0].r.baseVal.value;
-    const halfAngle = Math.atan(sideCenter / sideBottomHalf) / eyeRadius; //앵글 반쪽의 한 글자당 라디안 계산
-    const inputLength = e.target.value.length;
-
-    for (let i = 0; i < inputLength; i++) {
-        const x = Math.cos(halfAngle * (i+8)) * eyeRadius * -1 ;
-        const y = Math.sin(halfAngle * (i+8)) * eyeRadius - eyeRadius + 1;
-        eyes[0].style.transform = `translate(${x}px, ${y}px)`;
-        eyes[1].style.transform = `translate(${x}px, ${y}px)`;
-    }
-    if (inputLength == 0) {
-        eyes[0].style.transform = `none`;
-        eyes[1].style.transform = `none`;
+class Eye {
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
     }
 }
 
-// input.addEventListener('input', inputValueCheck);
-input.addEventListener('input', inputValueCheck2);
+//TODO: msgBox class 만들기
+function calcEyeIrisPosition(black, white, clientWidth, clientHeight, inputLength) {
+    const msgBoxY = clientHeight * msgBoxMarginUpDown;
+    const msgBoxPosition1 = {x: clientWidth * msgBoxMarginSide, y: msgBoxY};
+    const msgBoxCenterPosition = {x: clientWidth * (msgBoxMarginSide + msgBoxWidth / 2), y: msgBoxY};
+    const eyeTrajectoryRadius = white.radius - black.radius;
+
+    const sideCenter = msgBoxCenterPosition.y - white.y;
+    const sideBottomHalf = msgBoxCenterPosition.x - msgBoxPosition1.x;
+
+    const halfAngle = Math.atan(sideCenter / sideBottomHalf) / eyeTrajectoryRadius; //앵글 반쪽의 한 글자당 라디안 계산
+
+    const x = Math.cos(halfAngle * (inputLength + 8)) * eyeTrajectoryRadius * -1;
+    const y = Math.sin(halfAngle * (inputLength + 8)) * eyeTrajectoryRadius - eyeTrajectoryRadius + 1;
+    return {x, y};
+}
+
+function eyeMovingHandler(e, eyesB, eyesW, clientWidth, clientHeight) {
+    const inputLength = e.target.value.length;
+    if (inputLength === 0) {
+        eyesB[0].style.transform = `none`;
+        eyesB[1].style.transform = `none`;
+        return;
+    }
+
+    const black = new Eye(eyesB[0].cx.baseVal.value, eyesB[0].cy.baseVal.value, eyesB[0].r.baseVal.value);
+    const white = new Eye(eyesW[0].cx.baseVal.value, eyesW[0].cy.baseVal.value, eyesW[0].r.baseVal.value);
+    const {x, y} = calcEyeIrisPosition(black, white, clientWidth, clientHeight, inputLength);
+
+    eyesB[0].style.transform = `translate(${x}px, ${y}px)`;
+    eyesB[1].style.transform = `translate(${x}px, ${y}px)`;
+}
+
+msgBox.addEventListener('input', (e) => eyeMovingHandler(e, eyesB, eyesW, document.body.clientWidth, document.body.clientHeight));
